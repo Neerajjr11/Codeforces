aef217d96bd654cb2ae14d59535b5160e280c7a9
Neerajjr11 <neerajjm12345@gmail.com>
2022-11-07 18:22:00 +0530

    Add solution for problem `A - Dislike of Threes` Link: https://codeforces.com/contest/1560/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1560/submission/179968941

--- codeforces/1560/A.cpp
+++ codeforces/1560/A.cpp
@@ -1,3 +1,26 @@
+// A.Dislike of Threes
+//     time limit per test1 second
+//         memory limit per test256 megabytes
+//             inputstandard input
+//                 outputstandard output
+//                     Polycarp doesn't like integers that are divisible by 3 or end with the digit 3 in their decimal representation. Integers that meet both conditions are disliked by Polycarp, too.
+
+//     Polycarp starts to write out the
+//     positive(greater than 0) integers which he likes : 1,
+//     2, 4, 5, 7, 8, 10, 11, 14, 16,….Output the k - th element of this sequence(the elements are numbered from 1).
+
+//                                                    Input The first line contains one integer t(1≤t≤100) — the number of test cases.Then t test cases follow.
+
+//                                                    Each test case consists of one line containing one integer k(1≤k≤1000)
+//                                                        .
+
+//                                                    Output For each test case,
+//     output in a separate line one integer x — the k - th element of the sequence that was written out by Polycarp.
+
+//                                                       Example
+//                                                       inputCopy
+//                                                       10 1 2 3 4 5 6 7 8 9 1000 outputCopy
+//                                                       1 2 4 5 7 8 10 11 14 1666
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -202,31 +225,36 @@ void solve()
     cin >> n;
     int j = 1;
     int i = 1;
-    while (i != 0)
+    if (n == 1)
+        cout << 1 << endl;
+    else
     {
-        if (j == n)
+        while (i != 0)
         {
-            while (i != 0)
+            if (j == (n))
             {
-                if ((i % 3 != 0) && (i % 10 != 3))
+                while (i != 0)
                 {
-                    cout << i << endl;
-                    return;
-                }
-                else
-                {
-                    i++;
+                    if ((i % 3 != 0) && (i % 10 != 3))
+                    {
+                        cout << i << endl;
+                        return;
+                    }
+                    else
+                    {
+                        i++;
+                    }
                 }
             }
-        }
-        if ((i % 3 != 0) && (i % 10 != 3))
-        {
-            i++;
-            j++;
-        }
-        else
-        {
-            i++;
+            if ((i % 3 != 0) && (i % 10 != 3))
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                i++;
+            }
         }
     }
 }
